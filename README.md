b9b0178f5256a355257f8b67bd01fc9a3cb5af83
sumitsingh6 <sumitkr.singh98@gmail.com>
2023-08-07 00:32:41 +0530

    intial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Python Assignment: CRUD Api
+**A Flask application that performs CRUD (Create, Read, Update, Delete) operations on a MongoDB database for a User resource using a REST API**
+
+## The REST API
+Host: http://127.0.0.1:5000
+
+### 1. To fetch all the users from the database
+`GET /users`
+
+
+**Response**:
+
+    [{"_id":{"$oid":"id123"},"name":"user1","email":"user1@user1.com","password":"pass1"},{"_id":{"$oid":"id124"},"name":"user2","email":"user2@user2.com","password":"pass2"}]
+![enter image description here](https://user-images.githubusercontent.com/38861211/258647832-d1414852-08b8-4ad7-8dea-acf6cfd8bb5c.png)
+### 2. To return the user with the specified ID
+
+`GET /users/<id>`
+
+
+**Response**
+`[{"_id":"$oid":"id123"},"name":"user1","email":"user1@user1.com","password":"pass1"}]`
+
+![enter image description here](https://user-images.githubusercontent.com/38861211/258647712-4a046cc3-46be-4df6-a18c-406371a3568a.png)
+
+### 3. To create a new user with the specified data
+
+`POST /users`
+
+
+**Response**
+`{"message":"User added successfully"}`
+
+![enter image description here](https://user-images.githubusercontent.com/38861211/258647970-33d89bc9-26a3-4143-9bdd-2e1b4b4c2138.png)
+
+### 4. To update a user with the specified ID with the new data.
+
+`PUT /users/<id>`
+
+
+**Body**
+`{"name":"Test1","email":"test@testupdate.com","password":"password update"}`
+
+(only add fields that needs to be updated )
+
+
+**Response**
+`{"message":"User updated"}`
+
+![enter image description here](https://user-images.githubusercontent.com/38861211/258648933-ceb213ed-e5b3-4020-80cb-319b52636779.png)
+
+### 5. Deletes the user with the specified ID
+`DELETE /users/<id>`
+
+
+**Response:**
+`{"message":"User Deleted"}`
+
+![enter image description here](https://user-images.githubusercontent.com/38861211/258648713-f2cd3890-493c-41cc-ba74-aec719df2993.png)
---
+++ app.py
@@ -0,0 +1,120 @@
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+import json
+ 
+ 
+app = Flask(__name__)
+ 
+app.secret_key = '555e051a9f814b689dfe94d9ac554e862bfb5c04'
+ 
+app.config['MONGO_URI'] = "mongodb+srv://sumitkrsingh98:ROYdiCxgeAcn4O1R@cluster0.0cfl7xx.mongodb.net/corider_users?retryWrites=true&w=majority"
+ 
+mongo = PyMongo(app)
+ 
+db = mongo.db.users
+ 
+@app.route('/users', methods = ['POST'])
+def add_user():
+    _json = request.json
+    _name = _json['name']
+    _email = _json['email']
+    _password = _json['password']
+ 
+    if _name and _email and _password and request.method == 'POST':
+ 
+        _hashed_password = generate_password_hash(_password)
+        insert_result = db.insert_one({
+            'name': _name,
+            'email': _email,
+            'password': _hashed_password
+        })
+ 
+        # Access the inserted ID from the InsertOneResult object
+        inserted_id = str(insert_result.inserted_id)
+ 
+        # Convert the UpdateResult object to a Python dictionary
+        response_data = {
+            "acknowledged": insert_result.acknowledged,
+            "inserted_id": inserted_id
+        }
+
+        return jsonify(response_data)
+    else:
+        return not_found()
+ 
+
+#Get all users in databse
+@app.route('/users')
+def get_all_Users():
+    userdata = db.find()
+    resp = dumps(userdata)
+    return jsonify(json.loads(resp))
+ 
+ #Get a user by Id
+@app.route('/users/<id>')
+def get_user(id):
+    user = db.find_one({'_id': ObjectId(id)})
+    resp = dumps(user)
+    return jsonify(json.loads(resp))
+ 
+ 
+@app.route('/users/<id>', methods = ['PUT'])
+def update_user(id):
+    _json = request.json
+ 
+    if request.method == 'PUT':
+ 
+        change_set = {}
+ 
+        if 'name' in _json:
+            change_set['name'] = _json['name']
+ 
+        if 'email' in _json:
+            change_set['email'] = _json['email']
+ 
+        if 'password' in _json:
+            _hashed_password = generate_password_hash(_json['password'])
+            change_set['password'] = _hashed_password
+ 
+        objId = ObjectId(id)
+ 
+        update_result = db.update_one({'_id':objId}, {'$set': change_set})
+        # Convert the UpdateResult object to a Python dictionary
+        result_dict = {
+            "acknowledged": update_result.acknowledged,
+            "matched_count": update_result.matched_count,
+            "modified_count": update_result.modified_count,
+            "upserted_id": str(update_result.upserted_id) if update_result.upserted_id else None,
+        }
+        return jsonify(result_dict)
+ 
+ 
+@app.route('/users/<id>', methods = ['DELETE'])
+def delete_user(id):
+    delete_result = db.delete_one({'_id': ObjectId(id)})
+ 
+    # Construct a JSON response with the delete result information
+    response_data = {
+        "acknowledged": delete_result.acknowledged,
+        "deleted_count": delete_result.deleted_count,
+    }
+ 
+    # Use Flask's jsonify function to return the JSON response
+    return jsonify(response_data)
+ 
+@app.errorhandler(404)
+def not_found(error = True):
+    message = {
+        'status': 404,
+        'message': 'Not Found '+request.url
+    }
+ 
+    resp = jsonify(message)
+    resp.status_code = 404
+    return resp
+ 
+if __name__ == "__main__":
+    app.run(port=8000, host="0.0.0.0")
